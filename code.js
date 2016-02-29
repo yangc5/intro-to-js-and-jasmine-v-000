2f1cc7392633f64537227c67d7a41b3be8b67e25
Chen Yang <yangc5@mail.uc.edu>
2016-02-29 22:45:48 +0000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(icecream){
+  return "I love "+icecream;
+}
+
+function shouting(sentence){
+  return sentence.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
